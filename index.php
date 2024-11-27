1544b69ccabf0ab05beec0d9bed1987d91e609f9
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-27 16:04:08 +0100

    style: init tailwindcss

---
+++ index.php
@@ -0,0 +1,13 @@
+<Doctype html>
+<html>
+    <head>
+        <title>Pokemon</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body>
+        <h1 class="text-3xl font-bold underline">Pokemon</h1>
+
+    </body>
+</html>
