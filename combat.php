3fbd6438828c02a20e0a7033e7c88d334980af03
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-29 10:24:35 +0100

    chore: combat & select

--- combat.php
+++ combat.php
@@ -15,13 +15,28 @@ require_once './class/Attaque.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-black text-white w-full h-screen flex flex-col items-center gap-10 justify-around py-20">
+<div class="mx-auto">
+    <h1 class="text-3xl font-medium text-center">C'est l'heure de<br/><span class="font-bold">Combattre <img src="assets/pokeball.svg"
+                                                                                        alt="Pokeball" class="inline-block w-6 h-6"></span>
+    </h1>
+</div>
 
-<h1 class="text-3xl font-bold underline">Combat Pokemon</h1>
+<div class="flex gap-10 items-center">
+    <img src="./assets/tortank.png" alt="Pokemon 1" class="inline-block w-52 h-52">
+    <div class="bg-gray-300/50 rounded-lg p-2 w-fit"><p>V/S</p></div>
+    <img src="./assets/drakofeu.png" alt="Pokemon 2" class="inline-block w-52 h-52">
+
+</div>
 
 <?php
 
-echo '<h2>'. $_GET["pokemon1"] . '</h2>';
+$pokemon1 = $_POST['pokemon1'];
+$pokemon2 = $_POST['pokemon2'];
+
+echo $pokemon1;
+echo $pokemon2;
+
 
 
 
--- index.php
+++ index.php
@@ -13,61 +13,48 @@ require_once './class/Attaque.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-<h1 class="text-3xl font-bold underline">Pokemons</h1>
-
+<body class="bg-black text-white w-full h-screen flex flex-col items-center gap-10 justify-around py-20">
+<div class="mx-auto">
+    <h1 class="text-3xl font-bold text-center">Sélectionne tes <br/><span>Pokemons <img src="./assets/pokeball.svg"
+                                                                                      alt="Pokeball" class="inline-block w-6 h-6"></span>
+    </h1>
+</div>
 <?php
 $json = file_get_contents('./pokemons.json');
 $pokemonsData = json_decode($json, true);
-
-foreach ($pokemonsData['types'] as $type => $pokemonsList) {
-    echo '<h2 class="text-2xl font-bold underline">' . $type . '</h2>';
-    foreach ($pokemonsList as $pokemon) {
-        echo '<h3 class="text-xl font-bold underline">' . $pokemon['pokemon'] . '</h3>';
-        echo '<ul>';
-        foreach ($pokemon['attaques'] as $attaque) {
-            echo '<li>' . $attaque['nom'] . ' : ' . $attaque['degats'] . ' / ' . $attaque['precision'] . '</li>';
-        }
-        echo '</ul>';
-    }
-}
-
-$pokemon = new PokemonFeu('Salamèche', 39, 10, 2);
-echo '<h2>' . $pokemon->getNom() . '</h2>';
-echo '<p>Points de vie : ' . $pokemon->getPointsDeVie() . '</p>';
-echo '<p>Capacité spéciale : ' . $pokemon->getNomCapaciteSpeciale() . '</p>';
-
-$pokemon2 = new PokemonPlante('Bulbizarre', 45, 4, 1);
-echo '<h2>' . $pokemon2->getNom() . '</h2>';
-$attaque = new Attaque('Lance-Flammes', 90, 100);
-
-echo '<p>Pokemon 1 attaque Pokemon 2</p>';
-echo '<p>Pokemon 2 a ' . $pokemon2->getPointsDeVie() . '</p>';
-$pokemon->capaciteSpeciale($pokemon2);
-echo '<p>Points de vie : ' . $pokemon2->getPointsDeVie() . '</p>';
-
 ?>
 
-<form action="combat.php" method="post" id="pokemons_form">
-    <select name="pokemon1" required class="ml-4 border-2 border-gray-300 rounded-lg py-2 px-4">
-        <?php
-        foreach ($pokemonsData['types'] as $type => $pokemonsList) {
-            foreach ($pokemonsList as $pokemon) {
-                echo '<option value="' . $pokemon['pokemon'] . '">' . $pokemon['pokemon'] . '</option>';
+<form action="combat.php" method="post" id="pokemons_form" class="flex flex-col gap-4 items-center">
+    <div class="flex gap-10 justify-center">
+        <select name="pokemon1" id="pokemon1" required
+                class="ml-4 border-2 border-gray-300 rounded-lg py-2 px-10 text-gray-700">
+            <?php
+            foreach ($pokemonsData['types'] as $type => $pokemonsList) {
+                foreach ($pokemonsList as $pokemon) {
+                    echo '<option value="' . $pokemon['pokemon'] . '">' . $pokemon['pokemon'] . '</option>';
+                }
             }
-        }
-        ?>
-    </select>
-    <select name="pokemon2" required class="ml-4 border-2 border-gray-300 rounded-lg py-2 px-4">
-        <?php
-        foreach ($pokemonsData['types'] as $type => $pokemonsList) {
-            foreach ($pokemonsList as $pokemon) {
-                echo '<option value="' . $pokemon['pokemon'] . '">' . $pokemon['pokemon'] . '</option>';
+            ?>
+        </select>
+        <div class="bg-gray-300/50 rounded-lg p-2 w-fit"><p>V/S</p></div>
+        <select name="pokemon2" id="pokemon2" required
+                class="ml-4 border-2 border-gray-300 rounded-lg py-2 px-10 text-gray-700">
+            <?php
+            foreach ($pokemonsData['types'] as $type => $pokemonsList) {
+                foreach ($pokemonsList as $pokemon) {
+                    echo '<option value="' . $pokemon['pokemon'] . '">' . $pokemon['pokemon'] . '</option>';
+                }
             }
-        }
-        ?>
-    </select>
-    <input type="submit" value="Lancer" class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+            ?>
+        </select>
+    </div>
+    <button type="submit" value="Lancer"
+            class=" bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline w-fit">
+        Lancer le combat
+    </button>
 </form>
+
+
+
 </body>
 </html>
